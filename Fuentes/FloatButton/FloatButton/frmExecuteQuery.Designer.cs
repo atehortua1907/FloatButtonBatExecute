0a2d0e3495872552ade74eb8c4f9a1b2e6093243
jdatehortua <jdatehortua@inaltecsoft.com>
2020-07-31 17:39:54 -0500

    Subiendo proyecto

---
+++ Fuentes/FloatButton/FloatButton/frmExecuteQuery.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace FloatButton
+{
+    partial class frmExecuteQuery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecuteQuery = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExecuteQuery
+            // 
+            this.btnExecuteQuery.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnExecuteQuery.Location = new System.Drawing.Point(0, 0);
+            this.btnExecuteQuery.Name = "btnExecuteQuery";
+            this.btnExecuteQuery.Size = new System.Drawing.Size(184, 111);
+            this.btnExecuteQuery.TabIndex = 0;
+            this.btnExecuteQuery.Text = "Execute Query";
+            this.btnExecuteQuery.UseVisualStyleBackColor = true;
+            this.btnExecuteQuery.Click += new System.EventHandler(this.btnExecuteQuery_Click);
+            // 
+            // frmExecuteQuery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 111);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnExecuteQuery);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(200, 150);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(200, 150);
+            this.Name = "frmExecuteQuery";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "  ";
+            this.Load += new System.EventHandler(this.frmExecuteQuery_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecuteQuery;
+    }
+}
---
+++ Fuentes/FloatButton/FloatButton/frmExecuteQuery.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FloatButton
+{
+    public partial class frmExecuteQuery : Form
+    {
+        public frmExecuteQuery()
+        {
+            InitializeComponent();
+        }
+
+        private void frmExecuteQuery_Load(object sender, EventArgs e)
+        {
+            this.btnExecuteQuery.Text = ConfigurationManager.AppSettings["TextButton"].ToString();
+        }
+
+
+        private void btnExecuteQuery_Click(object sender, EventArgs e)
+        {
+            Process proc = null;
+            try
+            {
+
+                string batDir = Path.GetFullPath(ConfigurationManager.AppSettings["FilePathQuery"].ToString());
+                proc = new Process();
+                proc.StartInfo.WorkingDirectory = batDir;
+                proc.StartInfo.FileName = ConfigurationManager.AppSettings["FileName"].ToString();
+                proc.StartInfo.CreateNoWindow = false;
+                proc.Start();
+                proc.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace.ToString());
+            }
+        }
+
+    }
+}
